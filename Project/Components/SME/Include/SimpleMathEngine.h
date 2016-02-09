9a808746bf56ca1be9873709151aef793fcec11b
Felipe Sakano <felipe.sakano@gmail.com>
2016-02-09 21:54:15 -0200

    Added SimpleMathEngine (SME) project

---
+++ Project/Components/SME/Include/SimpleMathEngine.h
@@ -0,0 +1,21 @@
+#ifndef SME_SimpleMathEngine_H
+#define SME_SimpleMathEngine_H
+
+
+// Simple Math Engine namespace
+namespace SME
+{
+#if defined(_WIN32) || defined(__WIN32__)
+	#ifdef SIMPLE_MATH_ENGINE_API_EXPORT
+		#define SIMPLE_MATH_ENGINE_API __declspec(dllexport)
+		#define SIMPLE_MATH_ENGINE_EXP_TEMPLATE
+	#else
+		#define SIMPLE_MATH_ENGINE_API __declspec(dllimport)
+		#define SIMPLE_MATH_ENGINE_EXP_TEMPLATE extern
+	#endif
+#else
+	#error Platform not supported...
+#endif
+};
+
+#endif // SME_SimpleMathEngine_H
