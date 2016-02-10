841939b95d255af901fa428dbcbd46a85d93d18d
Felipe Sakano <felipe.sakano@gmail.com>
2016-02-10 05:42:48 -0200

    Added Constants and functions header files

---
+++ Project/Components/SME/Include/Contants.h
@@ -0,0 +1,10 @@
+#ifndef MathEngine_Constants_H
+#define MathEngine_Constants_H
+
+namespace MathEngine
+{
+	const float c_pi	= (float)(4.0 * atan(1.0));
+	const float c_twoPi	= c_pi * 2.0f;
+};
+
+#endif // MathEngine_Constants_H
---
+++ Project/Components/SME/Include/Functions.h
@@ -0,0 +1,12 @@
+#ifndef MathEngine_Functions_H
+#define MathEngine_Functions_H
+
+// MAth Engine Includes
+#include "SimpleMathEngineSetup.h"
+
+namespace MathEngine
+{
+	inline float RadiansToDegrees(float radians) { return (radians * 180.0f) / 3.14159265358979323846f; }
+};
+
+#endif // MathEngine_Functions_H
