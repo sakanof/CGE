dc983fd5593121ae1bf34fac3cfc8c91657ee3e4
Felipe Sakano <felipe.sakano@gmail.com>
2016-02-13 00:23:30 -0200

    Added the Resource class

---
+++ Project/Components/ResourceEngine/Include/Resource.h
@@ -0,0 +1,32 @@
+#ifndef ResourceEngine_Resource_H
+#define ResourceEngine_Resource_H
+
+// Standard Includes
+#include <string>
+
+// Utilities Includes
+#include <Utilities\Include\File.h>
+
+// ResourceEngine Includes
+#include "ResourceEngineSetup.h"
+
+namespace ResourceEngine
+{
+	class RESOURCE_ENGINE_API Resource
+	{
+	private:
+		std::string m_fileName;
+		std::string m_filePath;
+		std::string m_fileExtension;
+
+	public:
+		Resource(std::string filePath);
+
+	public:
+		std::string GetFileName() const;
+		std::string GetFilePath() const;
+		std::string GetFileExtension() const;
+	};
+};
+
+#endif // ResourceEngine_Resource_H
---
+++ Project/Components/ResourceEngine/Source/Resource.cpp
@@ -0,0 +1,15 @@
+#include "../Include/Resource.h"
+
+namespace ResourceEngine
+{
+	Resource::Resource(std::string filePath)
+	{
+		this->m_fileName = Utilities::File::GetFileName(filePath);
+		this->m_filePath = Utilities::File::GetFileAbsolutePath(filePath);
+		this->m_fileExtension = Utilities::File::GetFileExtension(filePath);
+	}
+
+	std::string Resource::GetFileName() const { return this->m_fileName; }
+	std::string Resource::GetFilePath() const{ return this->m_filePath;  }
+	std::string Resource::GetFileExtension() const{ return this->m_fileExtension;  }
+};
