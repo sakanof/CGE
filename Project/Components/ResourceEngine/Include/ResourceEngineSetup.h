9108a44137e99d6b40d724d40914bc8e1848d146
Felipe Sakano <felipe.sakano@gmail.com>
2016-02-13 00:15:40 -0200

    Revert "Explicit exporting std::string class in the ResourceEngine component"
    This reverts commit 8c50eaf65e23abd980ee83f9b5b9231252937cc4.

--- Project/Components/ResourceEngine/Include/ResourceEngineSetup.h
+++ Project/Components/ResourceEngine/Include/ResourceEngineSetup.h
@@ -14,17 +14,6 @@ namespace ResourceEngine
 #else
 	#error Platform not supported...
 #endif
-
-	/**********************************************************************
-	|	Provide the storage class specifier (extern for an .exe file, null
-	|	for DLL) and the __declspec specifier (dllimport for .an .exe file,
-	|	dllexport for DLL).
-	|	You must define UTILITIES_API_EXPORT when compiling the DLL.
-	|	You can now use this header file in both the .exe file and DLL - a
-	|	much safer means of using common declarations than two different
-	|	header files.
-	**********************************************************************/
-	RESOURCE_ENGINE_EXP_TEMPLATE template class RESOURCE_ENGINE_API std::basic_string<char, std::char_traits<char>, std::allocator<char>>;
 };
 
 #endif // ResourceEngineSetup_H
