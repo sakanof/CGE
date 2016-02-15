4959164a78a2229df84f61c5fabe38cb1e5af453
Felipe Sakano <felipe.sakano@gmail.com>
2016-02-14 23:47:37 -0200

    Added SME include file for all its include files

---
+++ Project/Components/SME/Include/SME.h
@@ -0,0 +1,12 @@
+#ifndef SME_H
+#define SME_H
+
+#include "Contants.h"
+#include "Functions.h"
+#include "Definitions.h"
+#include "Vector.h"
+#include "Matrix.h"
+#include "Quaternion.h"
+#include "Transform.hpp"
+
+#endif // SME_H
