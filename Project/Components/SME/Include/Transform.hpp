50cc9c019e5ad691b9c586b4f64a7fa65182511e
Felipe Sakano <felipe.sakano@gmail.com>
2016-02-14 23:49:17 -0200

    Moving the implementation of the Transform class to its hpp file

--- Project/Components/SME/Include/Transform.hpp
+++ Project/Components/SME/Include/Transform.hpp
@@ -3,10 +3,13 @@
 
 // SME Inlcudes
 #include "Definitions.h"
+#include "Vector.h"
+#include "Matrix.h"
+#include "Quaternion.h"
 
 namespace SME
 {
-	class SIMPLE_MATH_ENGINE_API Transform
+	class Transform
 	{
 	private:
 		Vec3		m_position;
@@ -14,30 +17,52 @@ namespace SME
 		float		m_scale;
 
 	public:
-		Transform(const Vec3& position = Vec3(0.0f, 0.0f, 0.0f), const Quaternion& rotation = Quaternion(0.0f, 0.0f, 0.0f, 1.0f), float scale = 1.0f);
+		Transform(const Vec3& position, const Quaternion& rotation, float scale)
+			: m_position(position),
+			  m_rotation(rotation),
+			  m_scale(scale) {}
 
-		inline Mat4 GetTransformation() const;
-		inline void LookAt(const Vec3& point, const Vec3& up);
+		inline Mat4 GetTransformation() const
+		{
+			Mat4 translationMatrix(1.0f);
+			Mat4 scaleMatrix(1.0f);
 
-		inline Quaternion GetLookAtRotation(const Vec3& point, const Vec3& up);
+			translationMatrix = translationMatrix.Translate(this->m_position);
 
-		inline Vec3		  GetPosition() const;
-		inline Quaternion GetRotation() const;
-		inline float      GetScale()    const;
+			if (this->m_scale != 1.0f)
+				scaleMatrix = scaleMatrix.Scale(this->m_scale);
 
-		inline void SetPosition(float x, float y, float z);
-		inline void SetPosition(const Vec3& pos);
-		inline void SetRotation(const Quaternion& rot);
-		inline void SetScale(float scale);
+			Mat4 result = translationMatrix * this->m_rotation.ToRotationMatrix() * scaleMatrix;
 
-		inline void Translate(const Vec3& translation);
-		inline void Translate(float x, float y, float z);
-		inline void Rotate(const Vec3& axis, float angle);
-		inline void Rotate(const Quaternion& rotation);
-		inline void Scale(float scale);
+			return result;
+		}
+		
+		inline void LookAt(const Vec3& point, const Vec3& up) { this->m_rotation = GetLookAtRotation(point, up); }
+
+		inline Quaternion GetLookAtRotation(const Vec3& point, const Vec3& up)
+		{
+			return Quaternion(Mat4().RotateFromDirection(Vec3(point.x - m_position.x, point.y - m_position.y, point.z - m_position.z).Normalize(), up));
+		}
+
+		inline Vec3 GetPosition() const { return this->m_position; }
+		
+		inline Quaternion GetRotation() const { return this->m_rotation; }
+		
+		inline float GetScale() const { return this->m_scale; }
+
+		inline void SetPosition(float x, float y, float z) { this->m_position = Vec3(x, y, z); }
+		inline void SetPosition(const Vec3& pos) { this->m_position = pos; }
+		inline void SetRotation(const Quaternion& rot) { this->m_rotation = rot; }
+		inline void SetScale(float scale) { this->m_scale = scale; }
+
+		inline void Translate(const Vec3& translation) { this->m_position += translation; }
+		inline void Translate(float x, float y, float z) { this->m_position += Vec3(x, y, z); }
+		inline void Rotate(const Vec3& axis, float angle) { Rotate(Quaternion(axis, angle)); }
+		inline void Rotate(const Quaternion& rotation) { this->m_rotation = Quaternion((rotation * this->m_rotation).Normalize()); }
+		inline void Scale(float scale) { this->m_scale += scale; }
 	};
 }; // namespace SME
 
-#include "Transform.inl"
+//#include "Transform.inl"
 
 #endif // SME_Transform_H
