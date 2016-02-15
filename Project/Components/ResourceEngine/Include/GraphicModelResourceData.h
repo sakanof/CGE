28201221dc116c68d2d53a115a69d7ce7ceccfd4
Felipe Sakano <felipe.sakano@gmail.com>
2016-02-15 01:59:30 -0200

    Added GraphicModelResourceData class

---
+++ Project/Components/ResourceEngine/Include/GraphicModelResourceData.h
@@ -0,0 +1,41 @@
+#ifndef ResourceEngine_Data_GraphicModelResourceData_H
+#define ResourceEngine_Data_GraphicModelResourceData_H
+
+// Standard Includes
+#include <memory>
+
+// GameEngine Includes
+#include "IResourceData.h"
+#include "MeshNodeResourceData.h"
+#include "MaterialResourceData.h"
+
+namespace ResourceEngine
+{
+	namespace Data
+	{
+		class RESOURCE_ENGINE_API GraphicModelResourceData : public IResourceData
+		{
+		private:
+			SharedMeshNodeResourceData m_meshNode;
+			MaterialResourceDataVector m_materialList;
+
+		public:
+			GraphicModelResourceData(SharedMeshNodeResourceData m_meshNode, MaterialResourceDataVector m_materialList);
+			~GraphicModelResourceData();
+
+		public:
+			unsigned int Size() const;
+			std::string  Type() const;
+
+			SharedMeshNodeResourceData GetMeshNode(void) const;
+			MaterialResourceDataVector GetMaterialList(void) const;
+		};
+		ExplicitExportDataContainers(GraphicModelResourceData)
+
+		using SharedGraphicModelResourceData = std::shared_ptr < GraphicModelResourceData >;
+		using GraphicModelResourceDataVector = std::vector < std::shared_ptr<GraphicModelResourceData> >;
+		using GraphicModelResourceDataList   = std::list < std::shared_ptr<GraphicModelResourceData> >;
+	}; 
+}; 
+
+#endif // ResourceEngine_Data_GraphicModelResourceData_H
---
+++ Project/Components/ResourceEngine/Source/GraphicModelResourceData.cpp
@@ -0,0 +1,29 @@
+#include "../include/GraphicModelResourceData.h"
+
+namespace ResourceEngine
+{
+	namespace Data
+	{
+		GraphicModelResourceData::GraphicModelResourceData(SharedMeshNodeResourceData meshNode, MaterialResourceDataVector materialList)
+			: m_meshNode(meshNode), 
+			  m_materialList(materialList) {}
+		GraphicModelResourceData::~GraphicModelResourceData() { this->m_materialList.clear(); }
+
+		unsigned int GraphicModelResourceData::Size() const 
+		{
+			unsigned int mySize = 0;
+
+			mySize += this->m_meshNode->Size();
+
+			unsigned int materialIndex;
+			for (materialIndex = 0; materialIndex < this->m_materialList.size(); ++materialIndex)
+				mySize += this->m_materialList[materialIndex]->Size();
+
+			return mySize;
+		}
+		std::string GraphicModelResourceData::Type() const { return std::string("GraphicModelResourceData"); }
+
+		SharedMeshNodeResourceData GraphicModelResourceData::GetMeshNode(void) const { return this->m_meshNode; }
+		MaterialResourceDataVector GraphicModelResourceData::GetMaterialList(void) const { return this->m_materialList; }
+	};
+}; 
