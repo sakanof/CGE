e8abb8da4b200ffc743da9bffe5d5a2a74764aa3
Felipe Sakano <felipe.sakano@gmail.com>
2016-02-21 12:27:40 -0300

    Turning IInputEvent interface in a abstract class by adding a Time (std::chrono::time_point <std::chrono::system_clock>) attribute

--- Project/Components/WindowSystem/Include/IInputEvent.h
+++ Project/Components/WindowSystem/Include/IInputEvent.h
@@ -3,17 +3,34 @@
 
 // Standard Includes
 #include <string>
+#include <chrono>
+
+// WindowSystem Includes
+#include "WindowSystemSetup.h"
 
 namespace WindowSystem
 {
 	namespace Input
 	{
-		class IInputEvent
+		using std::chrono::time_point;
+		using std::chrono::system_clock;
+		using std::chrono::high_resolution_clock;
+
+		using Time = time_point < system_clock > ;
+
+		WINDOW_SYSTEM_EXP_TEMPLATE template class WINDOW_SYSTEM_API std::chrono::duration<std::chrono::system_clock::rep, std::chrono::system_clock::period>;
+		WINDOW_SYSTEM_EXP_TEMPLATE template class WINDOW_SYSTEM_API time_point<system_clock>;
+
+		class WINDOW_SYSTEM_API IInputEvent
 		{
+		protected:
+			Time m_time;
+
 		public:
-			IInputEvent() {}
+			IInputEvent(Time eventTime) : m_time(eventTime) {}
 			virtual ~IInputEvent() {}
 
+			Time GetEventTime() const { return this->m_time; }
 			virtual std::string Type() const = 0;
 		};
 	};
