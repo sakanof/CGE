be1be4e49574681f2fa140ea95631c49a6fb76de
Felipe Sakano <felipe.sakano@gmail.com>
2016-03-14 01:45:19 -0300

    Added more testing cases to the Vec2 class

--- Project/Tests/SMEUnitTesting/Source/Vec2Tests.cpp
+++ Project/Tests/SMEUnitTesting/Source/Vec2Tests.cpp
@@ -14,12 +14,36 @@ TEST(SME_Vector2, Addition)
 	EXPECT_FLOAT_EQ(v1.y + v2.y, result.y);
 }
 
+TEST(SME_Vector2, AdditionEquals)
+{
+	SME::Vec2 v1(+1.838f, +2.687f);
+	SME::Vec2 v2(+5.357f, -4.095f);
+
+	auto result = v1;
+	result += v2;
+
+	EXPECT_FLOAT_EQ(v1.x + v2.x, result.x);
+	EXPECT_FLOAT_EQ(v1.y + v2.y, result.y);
+}
+
 TEST(SME_Vector2, Subtraction)
 {
 	SME::Vec2 v1(-746.354f, +2.751f);
 	SME::Vec2 v2(+5479.718f, +55.871f);
 
 	auto result = v1 - v2;
+
+	EXPECT_FLOAT_EQ(v1.x - v2.x, result.x);
+	EXPECT_FLOAT_EQ(v1.y - v2.y, result.y);
+}
+
+TEST(SME_Vector2, SubtractionEquals)
+{
+	SME::Vec2 v1(-746.354f, +2.751f);
+	SME::Vec2 v2(+5479.718f, +55.871f);
+
+	auto result = v1;
+	result -= v2;
 
 	EXPECT_FLOAT_EQ(v1.x - v2.x, result.x);
 	EXPECT_FLOAT_EQ(v1.y - v2.y, result.y);
@@ -36,6 +60,18 @@ TEST(SME_Vector2, ScalarMultiplcation)
 	EXPECT_FLOAT_EQ(v1.y * scalar, result.y);
 }
 
+TEST(SME_Vector2, ScalarMultiplcationEquals)
+{
+	SME::Vec2 v1(+138.947f, -3517.648f);
+	float scalar = 16.178f;
+
+	auto result = v1;
+	result      *= scalar;
+
+	EXPECT_FLOAT_EQ(v1.x * scalar, result.x);
+	EXPECT_FLOAT_EQ(v1.y * scalar, result.y);
+}
+
 TEST(SME_Vector2, Multiplication)
 {
 	SME::Vec2 v1(-746.354f, +2.751f);
@@ -47,12 +83,36 @@ TEST(SME_Vector2, Multiplication)
 	EXPECT_FLOAT_EQ(v1.y * v2.y, result.y);
 }
 
+TEST(SME_Vector2, MultiplicationEquals)
+{
+	SME::Vec2 v1(-746.354f, +2.751f);
+	SME::Vec2 v2(+5479.718f, +55.871f);
+
+	auto result = v1;
+	result      *= v2;
+
+	EXPECT_FLOAT_EQ(v1.x * v2.x, result.x);
+	EXPECT_FLOAT_EQ(v1.y * v2.y, result.y);
+}
+
 TEST(SME_Vector2, Divition)
 {
 	SME::Vec2 v1(+5479.718f, +55.871f);
 	float scalar = 16.178f;
 
 	auto result = v1 / scalar;
+
+	EXPECT_FLOAT_EQ(v1.x / scalar, result.x);
+	EXPECT_FLOAT_EQ(v1.y / scalar, result.y);
+}
+
+TEST(SME_Vector2, DivitionEquals)
+{
+	SME::Vec2 v1(+5479.718f, +55.871f);
+	float scalar = 16.178f;
+
+	auto result = v1;
+	result      /= scalar;
 
 	EXPECT_FLOAT_EQ(v1.x / scalar, result.x);
 	EXPECT_FLOAT_EQ(v1.y / scalar, result.y);
