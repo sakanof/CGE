99649a7d4cd65c4439c93ee76587551f57db14f0
Felipe Sakano <felipe.sakano@gmail.com>
2016-03-13 22:33:21 -0300

    Added unit tests for the Utilities File functions

---
+++ Project/Tests/UtilitiesTest/Source/FileTest.cpp
@@ -0,0 +1,35 @@
+// Utilities Includes
+#include <Utilities\Source\File.cpp>
+#include <Utilities\Source\BaseException.cpp>
+#include <Utilities\Source\FileNotFoundException.cpp>
+#include <Utilities\Source\File.cpp>
+
+// Google Test Includes
+#include <gtest\gtest.h>
+
+TEST(Utilities_File, GetFileExtension)
+{
+	std::string filePath = "C:\\CGE\\Project\\Resources\\myFile.txt";
+	std::string extension = "txt";
+	std::string result = Utilities::File::GetFileExtension(filePath);
+
+	EXPECT_EQ(extension, result);
+}
+
+TEST(Utilities_File, GetFileName)
+{
+	std::string filePath = "C:\\CGE\\Project\\Resources\\myFile.txt";
+	std::string filename = "myFile.txt";
+	std::string result = Utilities::File::GetFileName(filePath);
+
+	EXPECT_EQ(filename, result);
+}
+
+TEST(Utilities_File, GetFileAbsolutePath)
+{
+	std::string filePath = "C:\\CGE\\Project\\Resources\\myFile.txt";
+	std::string absolutePath = "C:\\CGE\\Project\\Resources";
+	std::string result = Utilities::File::GetFileAbsolutePath(filePath);
+
+	EXPECT_EQ(absolutePath, result);
+}
