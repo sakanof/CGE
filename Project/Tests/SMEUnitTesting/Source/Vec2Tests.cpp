e0a371b3c200d95e5bcec006a0e07d8f760cc697
Felipe Sakano <felipe.sakano@gmail.com>
2016-03-14 01:01:17 -0300

    Added Vec2 Addition test case

--- Project/Tests/SMEUnitTesting/Source/Vec2Tests.cpp
+++ Project/Tests/SMEUnitTesting/Source/Vec2Tests.cpp
@@ -1,1 +1,15 @@
-#include <SME
+#include <SME\Include\Vector.h>
+
+// Google Test Includes
+#include <gtest\gtest.h>
+
+TEST(SME_Vector2, Addition)
+{
+	SME::Vec2 v1(+1.838f, +2.687f);
+	SME::Vec2 v2(+5.357f, -4.095f);
+
+	auto result = v1 + v2;
+
+	EXPECT_FLOAT_EQ(v1.x + v2.x, result.x);
+	EXPECT_FLOAT_EQ(v1.y + v2.y, result.y);
+}
